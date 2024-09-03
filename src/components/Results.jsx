71a5d10af4cda1ebb3d8fd48f3e022e2d6bae4fd
tishouzunov1 <46093632+tishouzunov1@users.noreply.github.com>
2024-09-03 11:11:10 +0300

    results component

---
+++ src/components/Results.jsx
@@ -0,0 +1,6 @@
+import { calculateInvestmentResults } from "../util/investment.js";
+export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+  console.log(resultsData);
+  return <p>Results...</p>;
+}
